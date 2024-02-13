21f2381d67a72a49c848432a5e89a23e991d99cf
Asif Khan <khanasif0870@gmail.com>
2024-02-13 21:50:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <h2>We are going to make a Calculator using Reactjs</h2>;
+}
+
+export default App;
